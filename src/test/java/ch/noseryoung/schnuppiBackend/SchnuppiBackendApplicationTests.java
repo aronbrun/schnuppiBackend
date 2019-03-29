8447c3851630ab77c8fea89ec633d0e4a6593a06
Aron Brun <aronbrun@gmail.com>
2019-03-29 14:52:25 +0100

    initialise Spring

---
+++ src/test/java/ch/noseryoung/schnuppiBackend/SchnuppiBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package ch.noseryoung.schnuppiBackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SchnuppiBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
